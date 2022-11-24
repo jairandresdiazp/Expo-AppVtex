cfef5efb38524a21f78505f2ac0386efe7a38837
Jair Andres Diaz Puentes <jairdiaz@blacksip.com>
2022-11-23 22:23:42 -0500

    inyect web view

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Platform, StyleSheet, View} from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import Spinner from 'react-native-loading-spinner-overlay';
 import WebView from 'react-native-webview';
 
 export default function App() {
-  const Url =
-    'https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d63141782.451334655!2d-2.970703!3d15!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sco!4v1669236929109!5m2!1ses!2sco';
+  const Url = 'http://vtex-store.blacksip.com/';
   const [loading, setLoading] = useState(true);
   const hideSpinner = () => {
     setLoading(false);
@@ -15,7 +15,7 @@ export default function App() {
     setLoading(true);
   };
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <Spinner
         color="#000"
@@ -23,35 +23,28 @@ export default function App() {
         textContent="Loading..."
         textStyle={styles.spinnerTextStyle}
       />
-      {Platform.OS === 'web' ? (
-        <iframe
-          src={Url}
-          height="100%"
-          width="100%"
-          onLoadStart={() => showSpinner()}
-          onLoad={() => hideSpinner()}
-          allowFullScreen
-          style={styles.iframe}
-        />
-      ) : (
-        <WebView
-          originWhitelist={['*']}
-          onLoadStart={() => showSpinner()}
-          onLoad={() => hideSpinner()}
-          bounces={false}
-          dataDetectorTypes="link"
-          scalesPageToFit={true}
-          scrollEnabled={false}
-          automaticallyAdjustContentInsets={false}
-          mediaPlaybackRequiresUserAction={true}
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-          startInLoadingState={true}
-          source={{ html: '<h1>Some Heading</h1>' }}
-          style={styles.webView}
-        />
-      )}
-    </View>
+      <WebView
+        originWhitelist={['*']}
+        onLoadStart={() => showSpinner()}
+        onLoad={() => hideSpinner()}
+        bounces={false}
+        dataDetectorTypes="link"
+        scalesPageToFit={true}
+        scrollEnabled={false}
+        automaticallyAdjustContentInsets={false}
+        mediaPlaybackRequiresUserAction={true}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        source={{ uri: Url }}
+        style={styles.webView}
+        containerStyle={styles.webView}
+        renderError={(error) => (
+          <View style={styles.container}>
+            <Text style={styles.webViewText}>Render Error: {error}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -71,8 +64,10 @@ const styles = StyleSheet.create({
   },
   webView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  webViewText: {
+    color: '#000',
   },
 });
